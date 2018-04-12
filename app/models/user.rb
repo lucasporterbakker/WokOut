fda3c7465d5ef99d80109285cd826c669634749a
Lucas Porter-Bakker <lucas.porter.bakker@gmail.com>
2018-04-12 11:23:15 +0800

    Added Carrierwave and Cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :meals# Include default devise modules. Others available are:
   validates :first_name, presence: true
   validates :last_name, presence: true
+  mount_uploader :photo, PhotoUploader
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
