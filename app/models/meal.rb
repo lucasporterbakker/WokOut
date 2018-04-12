a161fa1697c24bc16f3bf0ac803ccdabded04d27
Trey Booth <tbooth9@gmail.com>
2018-04-12 17:57:07 +0800

    still testing, don't pull

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,7 +2,7 @@ class Meal < ApplicationRecord
   has_many :bookings
   belongs_to :user
   validates :name, presence: true
-  validates :categroy, presence: true
-  validates :price, presence: true
+  # validates :category, presence: true
+  # validates :price, presence: true
   validates :description, presence: true
 end
