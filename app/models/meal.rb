befdb0bcc6430493248eb1b00d0f46c85dba2662
Martin Tannenberger <martin.tannenberger@gmail.com>
2018-04-12 13:09:18 +0800

    created booking model and routes

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,8 @@
 class Meal < ApplicationRecord
+  has_many :bookings
   belongs_to :user
+  validates :name, presence: true
+  validates :categroy, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
 end
